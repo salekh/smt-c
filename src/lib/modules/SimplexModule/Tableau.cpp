a25f5a0968d805842456a5c1f9afadfdc195ce9e
Sanchit Alekh <alekh@dbis.rwth-aachen.de>
2017-05-17 19:35:01 +0200

    Able to retrieve coefficients of each variable in each formula

--- src/lib/modules/SimplexModule/Tableau.cpp
+++ src/lib/modules/SimplexModule/Tableau.cpp
@@ -7,21 +7,42 @@ namespace smtrat
 	Tableau::Tableau(){}
 	
 	Tableau::Tableau(std::list<FormulaT> formulas){
+		
+		//accumulator set for all the variables in the formulae 
 		std::set<carl::Variable> variablesInFormula;
 		unsigned long number_of_formulas = formulas.size();
 
-		SMTRAT_LOG_ERROR("smtrat.my", number_of_formulas << " elements present in the formula");
+		SMTRAT_LOG_ERROR("smtrat.my", number_of_formulas << "Formulas");
+		
 		for(auto form : formulas)
 		{
 			//Get the variables in the formula
 			std::set<carl::Variable> x = form.variables();
+			//adding the variables into the accumulator set
 			variablesInFormula.insert(x.begin(),x.end());
+
+			/*
 			SMTRAT_LOG_ERROR("smtrat.my", "Constraint Value " << form.constraint().lhs().toString());
+			SMTRAT_LOG_ERROR("smtrat.my", "Value " << form.constraint().constantPart());
 			SMTRAT_LOG_ERROR("smtrat.my", "TABLEAU CREATE " << form.toString());
+			*/
 		}
-		for(auto var : variablesInFormula){
-			SMTRAT_LOG_ERROR("smtrat.my","Variables in Formulae " << var);
-		}
+
+		//get the coefficients of each variable in each formula
+		for(auto formula : formulas){
+			for(auto var : variablesInFormula){
+				if(formula.constraint().hasVariable(var)){
+					carl::MultivariatePolynomial<smtrat::Rational> coeff = formula.constraint().coefficient(var,1);
+					cout << coeff << "\t"; 
+				}
+				else{
+					cout << "0" <<  "\t";
+				}
+			}
+			cout << endl;
+		}	
+
+
 		unsigned long number_of_variables = variablesInFormula.size();
 		matrix.setConstant(number_of_formulas,number_of_variables,0);
 		SMTRAT_LOG_ERROR("smtrat.my", "Matrix: " << endl << matrix);
